3419de0636d2543293f18a562c311ce4aed04432
codeByAfzal <afzalxcode@gmail.com>
2025-08-30 22:29:58 +0530

    Added Seconds to All Unit Function

--- app.js
+++ app.js
@@ -1,39 +1,91 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const fromSelect = document.getElementById('from-units-select');
-    const toSelect = document.getElementById('to-units-select');
-    const input = document.getElementById('from-input');
-    const toInput = document.getElementById('to-input');
-    const reverseBtn = document.getElementById('reverse-icon');
-    const btns = document.querySelectorAll('.button');
-    const timeBtn = document.getElementById('time');
-    const lengthBtn = document.getElementById('length');
-    const temperatureBtn = document.getElementById('temperature');
-    const weightBtn = document.getElementById('weight');
+  const fromSelect = document.getElementById('from-units-select');
+  const toSelect = document.getElementById('to-units-select');
+  const input = document.getElementById('from-input');
+  const toInput = document.getElementById('to-input');
+  const reverseBtn = document.getElementById('reverse-icon');
+  const btns = document.querySelectorAll('.button');
+  const timeBtn = document.getElementById('time');
+  const lengthBtn = document.getElementById('length');
+  const temperatureBtn = document.getElementById('temperature');
+  const weightBtn = document.getElementById('weight');
 
-    reverseBtn.addEventListener('click', function() {
-        reverseBtn.classList.toggle('rotate');
+  let result;
 
-        // Reverse the Select Value
-        let tempValue = fromSelect.value;
+  reverseBtn.addEventListener('click', function() {
+      reverseBtn.classList.toggle('rotate');
+
+      // Reverse the Select Value
+      let tempValue = fromSelect.value;
         
-        fromSelect.value = toSelect.value;
-        toSelect.value = tempValue;
+      fromSelect.value = toSelect.value;
+      toSelect.value = tempValue;
 
-        // Reverse the Input Value
-        let tempInputValue = input.value;
+      // Reverse the Input Value
+      let tempInputValue = input.value;
 
-        input.value = toInput.value;
-        toInput.value = tempInputValue;
+      input.value = toInput.value;
+      toInput.value = tempInputValue;
 
-    })
+  })
 
-    // Button styling on click
-    btns.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        btns.forEach((b) => b.classList.remove("button-selected"));
-        btn.classList.add("button-selected");
-      });
+  // Button styling on click
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btns.forEach((b) => b.classList.remove("button-selected"));
+      btn.classList.add("button-selected");
     });
+  });
+
+
+  function convertTime() {
+
+    let toValue = parseFloat(input.value);
+
+    // Show Error
+    if(isNaN(toValue)) {
+      result = "Error";
+      toInput.classList.add('error');
+      toInput.value = result;
+    } else {
+      toInput.classList.remove('error');
+    }
+
+    // Second to all units
+    if(fromSelect.value === "second" && toSelect.value === "second") {
+      result = toValue;
+      toInput.value = parseFloat(result.toFixed(7));
+    } else if(fromSelect.value === "second" && toSelect.value === "millisecond") {
+      result = toValue * 1000;
+      toInput.value = parseFloat(result.toFixed(7));
+    } else if(fromSelect.value === "second" && toSelect.value === "minute") {
+      result = toValue / 60;
+      toInput.value = parseFloat(result.toFixed(7));
+    } else if(fromSelect.value === "second" && toSelect.value === "hour") {
+      result = toValue / 3600;
+      toInput.value = parseFloat(result.toFixed(7));
+    } else if(fromSelect.value === "second" && toSelect.value === "day") {
+      result = toValue / 86400;
+      toInput.value = parseFloat(result.toFixed(7));
+    } else if(fromSelect.value === "second" && toSelect.value === "week") {
+      result = toValue / 604800;
+      toInput.value = parseFloat(result.toFixed(8));
+    } else if(fromSelect.value === "second" && toSelect.value === "month") {
+      result = toValue / 2628000;
+      toInput.value = parseFloat(result.toFixed(9));
+    } else if(fromSelect.value === "second" && toSelect.value === "year") {
+      result = toValue / 31540000;
+      toInput.value = parseFloat(result.toFixed(10));
+    }
+
+    
+
+  }
+
+  setInterval(() => {
+    convertTime();
+  }, 100);
+
     
 })
